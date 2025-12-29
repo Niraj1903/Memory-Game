7a31f995b999ca063273a669346139893a3cbb0e
Niraj1903 <niraj.singh1903@outlook.com>
2025-12-30 01:23:20 +0530

    stateVariables created

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const MemoryGame = () => {
+  const [gridSize, setGridSize] = useState(4);
+  const [cards, setCards] = useState([]);
+  const [flipped, setFlipped] = useState([]);
+  const [solved, setSolved] = useState([]);
+  const [disabled, setDisabled] = useState(false);
+  const [won, setWon] = useState(false);
+
+  const initialGame = () => {
+    const totalCards = gridSize * gridSize;
+    const pairCount = Math.floor(totalCards / 2);
+    const numbers = [...Array(pairCount).keys()].map((n) => n + 1);
+    const shuffledCards = [...numbers, ...numbers].sort();
+  };
   return (
     <>
-      <div className="bg-red-400">Memory</div>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-grey-100 p-4">
+        <h1 className="text-3xl font-bold mb-6">Memory Game</h1>
+        <div>
+          <label className="mr-2" htmlFor="gridSize">
+            Grid Size : (maz 10)
+          </label>
+          <input
+            className="border-2 border-gray-300 rounded px-2 py-1"
+            type="number"
+            id="gridSize"
+            min="2"
+            max="10"
+            value={gridSize}
+            onChange={(e) => setGridSize(e.target.value)}
+          />
+        </div>
+      </div>
     </>
   );
 };
